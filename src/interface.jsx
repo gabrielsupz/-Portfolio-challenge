a498ff3c0ed2997d7228068495a4c41a24f0c37e
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-08-29 08:29:22 -0300

    React Preparado

---
+++ src/interface.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './interface.css'
+
+function Interface() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    
+   </div>
+  )
+}
+
+export default Interface
