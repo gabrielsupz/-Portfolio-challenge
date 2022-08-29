9a4e05b5a92e1dd2a2efc918185a250a5bf7145c
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-08-29 12:39:17 -0300

    Contact Finished

--- src/aside.jsx
+++ src/aside.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './css/aside.css'
 
+
 function Aside() {
   const [count, setCount] = useState(0)
 
@@ -12,7 +13,18 @@ function Aside() {
       <h2>Novice Developer</h2>
     </div>
     <div className='contact'>
-      <p>Contato</p>
+      <ul>
+        <li  id='contactFrase'> <img src="../public/assets/star.svg" alt="Ícon star" /><a>Contato</a></li>
+      <li> <img src="../public/assets/map-pin.svg" alt="Ícon map" /> <a href='https://goo.gl/maps/afDMc9DMK3PzCMoB8'>Sapiranga do RS, Brasil</a> </li>
+
+      <li><img src="../public/assets/linkedin.svg" alt="Ícon linkedin" /><a href='https://www.linkedin.com/in/gabriel-suptitz-51340124a/'>Gabriel Suptitz </a> </li>
+      <li><img src="../public/assets/github.svg" alt="Ícon github" /><a href='https://github.com/SuptitzGabriel'>SuptitzGabriel</a> </li>
+      <li><img src="../public/assets/instagram.svg" alt="Ícon instagram" /><a href='https://www.instagram.com/gabriel_supz/'>gabriel_supz</a></li>
+      <li><img src="../public/assets/mail.svg" alt="Ícon" /><a
+      href='mailto:gabrielsuptitz@gmail.com'>gabrielsuptitz@gmail.com</a></li>
+
+      
+      </ul>
     </div>
     <div className='exp'><p>Experiência</p></div>
     <div className='tec'><p>Tecnologias</p></div>
