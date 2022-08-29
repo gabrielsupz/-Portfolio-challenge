f2fee4081d15b25fcdca8782c7baa0612aa96939
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-08-29 08:30:09 -0300

    Commit Pré Início

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Interface from './interface'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Interface />
   </React.StrictMode>
 )
