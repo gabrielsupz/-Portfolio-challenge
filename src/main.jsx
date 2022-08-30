da1137490fefe786d888fe4bb79a7f8549e39c70
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-08-30 17:56:37 -0300

    Finished Messages

--- src/main.jsx
+++ src/main.jsx
@@ -14,48 +14,90 @@ function Main() {
       <div>
           <div className='project projectOne'>
           <div className='nameProject'>
-             <img src="../public/assets/folder.svg" alt="Ícon Folder" /><h4>-Portfolio-challenge</h4></div>  
+             <img src="../public/assets/folder.svg" alt="Ícon Folder" />
+             <a href='https://github.com/SuptitzGabriel/-Portfolio-challenge' target="blank">-Portfolio-challenge</a>
+          </div>  
              <p>Desafio da rockeseat,realizado com intuito de aprender mais sobre html e css,feito em react para treino.</p>
           <div>
             <div className='infoProject'>
-              <div className='infoGitHub'>   
-                <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" />
-                <p>001</p>
-                <img src="../public/assets/star-project.svg" alt="Ícon Star" />
-                <p>001</p>
+              <div className='infoGitHub'>  
+                <div> 
+                  <img src="../public/assets/star-project.svg" alt="Ícon Star" /> 
+                  <p>000</p>
+                </div>
+                <div>
+                  <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" />
+                  <p>001</p>
+                </div>
+
+               
               </div>
            
               <div className='projectTec'>
-                <p className='elipse'></p>
+                <p className='elipseJs'></p>
                 <p>JavaScript</p>
+                <p className='elipseHtml'></p>
+                <p>HTML</p>
+                <p className='elipseCss'></p>
+                <p>CSS</p>
               </div>
             </div>
                
              </div>
               </div>
-             <div className='project projectTwo'>
-            <h4>-Portfolio-challenge</h4>
-            {/* <img src="../public/assets/folder.svg" alt="Ícon Folder" /> */}
-          <p>Desafio da rockeseat,realizado com intuito de aprender mais sobre html e css,feito em react para treino.</p>
+              <div className='project projectTwo'>
+          <div className='nameProject'>
+             <img src="../public/assets/folder.svg" alt="Ícon Folder" />
+             <a href='https://github.com/SuptitzGabriel/NLW-Return-' target="blank">NLW-Return-</a>
+          </div>  
+             <p>Projeto realizado na NLW Return da Rocketseat com intuito de aprender o básico de html e css, criando um site simples.</p>
           <div>
-            <div>
-            {/* <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" /> */}
-            <p>1</p>
-            {/* <img src="../public/assets/star.svg" alt="Ícon Star" /> */}
-            <p>1</p>
+            <div className='infoProject'>
+              <div className='infoGitHub'>  
+                <div> 
+                  <img src="../public/assets/star-project.svg" alt="Ícon Star" /> 
+                  <p>000</p>
+                </div>
+                <div>
+                  <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" />
+                  <p>001</p>
+                </div>
+
+               
+              </div>
+           
+              <div className='projectTec'>
+              <p className='elipseJs'></p>
+                <p>JavaScript</p>
+                <p className='elipseHtml'></p>
+                <p>HTML</p>
+                <p className='elipseCss'></p>
+                <p>CSS</p>
+              </div>
             </div>
-            <p></p>
-          </div>
-          </div>
+               
+             </div>
+              </div>
         <div>
 
       </div>
      </div>
     </div>
-    <div className='recentPost'>
-      <h3>Recent Posts</h3>
+    <div className='message'>
+      <h3>Mensagem</h3>
     </div>
-    <div className='post'></div>
+    <div className='message textMessage'>
+<div><p className='messagePic'></p></div>
+      
+
+      <p>
+       <strong>Olá,Tudo certo✌️ ?</strong>  
+       <br />
+       <br />
+Meu nome é Gabriel Suptitz estou no caminho para me tornar um programador e tentando alcançar sempre um próximo nível 🚀, este é um portifólio feito com base no desafio do discover da Rocketseat!
+</p>
+       
+    </div>
   </main>
   )
 }
