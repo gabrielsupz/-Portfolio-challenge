c8466cc0f0c76084ae2128c941f74c29a44b1587
Gabriel Suptitz <gabrielsuptitz@gmail.com>
2022-08-31 15:59:25 -0300

    Document formated

--- src/aside.jsx
+++ src/aside.jsx
@@ -1,90 +1,139 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/aside.css'
 
-
-
 function Aside() {
-const [user, setUser] = useState({ name: "" , avatar: ""})
+  const [user, setUser] = useState({ name: '', avatar: '' })
 
-
-
-useEffect(() => {
-fetch("https://api.github.com/users/suptitzgabriel")
-.then(response => response.json() )
-.then(data => {
-  setUser({
-    name: data.name,
-    avatar: data.avatar_url
-  })
-})
-},[])
+  useEffect(() => {
+    fetch('https://api.github.com/users/suptitzgabriel')
+      .then(response => response.json())
+      .then(data => {
+        setUser({
+          name: data.name,
+          avatar: data.avatar_url
+        })
+      })
+  }, [])
   return (
-   <aside>
-    <div className='profile'>
-   <img id="gitImage" className='profilePic' src={user.avatar} alt="Imagem de perfil" />
-      <h1>{user.name}</h1>
-      <h2>Novice Developer</h2>
-    </div>
-    <div className='contact'>
-      <ul>
-        <li  id='contactFrase'> <img src="../public/assets/star.svg" alt="Ícon star" /><p>Contato</p></li>
-      <li> <img src="../public/assets/map-pin.svg" alt="Ícon map" /> <a href='https://goo.gl/maps/afDMc9DMK3PzCMoB8'>Sapiranga do RS, Brasil</a> </li>
-
-      <li><img src="../public/assets/linkedin.svg" alt="Ícon linkedin" /><a href='https://www.linkedin.com/in/gabriel-suptitz-51340124a/'>Gabriel Suptitz </a> </li>
-      <li><img src="../public/assets/github.svg" alt="Ícon github" /><a href='https://github.com/SuptitzGabriel'>SuptitzGabriel</a> </li>
-      <li><img src="../public/assets/instagram.svg" alt="Ícon instagram" /><a href='https://www.instagram.com/gabriel_supz/'>gabriel_supz</a></li>
-      <li><img src="../public/assets/mail.svg" alt="Ícon" /><a
-      href='mailto:gabrielsuptitz@gmail.com'>gabrielsuptitz@gmail.com</a></li>
-
-      
-      </ul>
-    </div>
-    <div className='tec'>
-      <h3>Tecnologias</h3>
-      <div className='technologies'>
-        <a title='Clique para ir ao MDN sobre HTML' href='https://developer.mozilla.org/pt-BR/docs/Web/HTML' target="blank">HTML5</a>
-        <a title='Clique para ir ao MDN sobre CSS' href='https://developer.mozilla.org/pt-BR/docs/Web/CSS' target="blank">CSS</a>
-        <a  title='Clique para ir ao MDN sobre React' href='https://developer.mozilla.org/pt-BR/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started' target="blank">REACTJS</a>
-        <a  title='Clique para ir ao MDN sobre JavaScript'href='https://developer.mozilla.org/pt-BR/docs/Web/JavaScript' target="blank">JAVASCRIPT</a>
-        <a title='Clique para ir ao MDN sobre node' href='https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs' target="blank">NODEJS</a>
+    <aside>
+      <div className="profile">
+        <img
+          id="gitImage"
+          className="profilePic"
+          src={user.avatar}
+          alt="Imagem de perfil"
+        />
+        <h1>{user.name}</h1>
+        <h2>Novice Developer</h2>
       </div>
-    </div>
-    <div className='exp'>
-      <h3>Experiência</h3>
-      <div>
+      <div className="contact">
         <ul>
-          <li><strong className='topic'>Mared Têxtil</strong> 
-            <ul className='infoLi'>
-              <li>2021-2022</li>
-              <li>Auxiliar de estoque.</li>
-            </ul>
-
+          <li id="contactFrase">
+            {' '}
+            <img src="../public/assets/star.svg" alt="Ícon star" />
+            <p>Contato</p>
           </li>
-        </ul>
-      </div>
-      
-    </div>
-    <div className='education'><h3>Educação</h3>
-      <div>
-        <ul>
           <li>
-            <strong className='topic'>Instituto Estadual Mathilde Zatar</strong> 
-            <ul className='infoLi'>
-              <li>
-                  2021-Atualmente
-              </li>
-            </ul>
-          
+            {' '}
+            <img src="../public/assets/map-pin.svg" alt="Ícon map" />{' '}
+            <a href="https://goo.gl/maps/afDMc9DMK3PzCMoB8">
+              Sapiranga do RS, Brasil
+            </a>{' '}
+          </li>
+
+          <li>
+            <img src="../public/assets/linkedin.svg" alt="Ícon linkedin" />
+            <a href="https://www.linkedin.com/in/gabriel-suptitz-51340124a/">
+              Gabriel Suptitz{' '}
+            </a>{' '}
+          </li>
+          <li>
+            <img src="../public/assets/github.svg" alt="Ícon github" />
+            <a href="https://github.com/SuptitzGabriel">SuptitzGabriel</a>{' '}
+          </li>
+          <li>
+            <img src="../public/assets/instagram.svg" alt="Ícon instagram" />
+            <a href="https://www.instagram.com/gabriel_supz/">gabriel_supz</a>
+          </li>
+          <li>
+            <img src="../public/assets/mail.svg" alt="Ícon" />
+            <a href="mailto:gabrielsuptitz@gmail.com">
+              gabrielsuptitz@gmail.com
+            </a>
           </li>
         </ul>
-       </div>
-    </div>
-   </aside>
+      </div>
+      <div className="tec">
+        <h3>Tecnologias</h3>
+        <div className="technologies">
+          <a
+            title="Clique para ir ao MDN sobre HTML"
+            href="https://developer.mozilla.org/pt-BR/docs/Web/HTML"
+            target="blank"
+          >
+            HTML5
+          </a>
+          <a
+            title="Clique para ir ao MDN sobre CSS"
+            href="https://developer.mozilla.org/pt-BR/docs/Web/CSS"
+            target="blank"
+          >
+            CSS
+          </a>
+          <a
+            title="Clique para ir ao MDN sobre React"
+            href="https://developer.mozilla.org/pt-BR/docs/Learn/Tools_and_testing/Client-side_JavaScript_frameworks/React_getting_started"
+            target="blank"
+          >
+            REACTJS
+          </a>
+          <a
+            title="Clique para ir ao MDN sobre JavaScript"
+            href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript"
+            target="blank"
+          >
+            JAVASCRIPT
+          </a>
+          <a
+            title="Clique para ir ao MDN sobre node"
+            href="https://developer.mozilla.org/pt-BR/docs/Learn/Server-side/Express_Nodejs"
+            target="blank"
+          >
+            NODEJS
+          </a>
+        </div>
+      </div>
+      <div className="exp">
+        <h3>Experiência</h3>
+        <div>
+          <ul>
+            <li>
+              <strong className="topic">Mared Têxtil</strong>
+              <ul className="infoLi">
+                <li>2021-2022</li>
+                <li>Auxiliar de estoque.</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="education">
+        <h3>Educação</h3>
+        <div>
+          <ul>
+            <li>
+              <strong className="topic">
+                Instituto Estadual Mathilde Zatar
+              </strong>
+              <ul className="infoLi">
+                <li>2021-Atualmente</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </aside>
   )
-
-
- 
-
 }
 
 export default Aside
--- src/main.jsx
+++ src/main.jsx
@@ -5,95 +5,122 @@ function Main() {
   const [count, setCount] = useState(0)
 
   return (
-  <main>
-    <div  className='myProject'>
-      <h3>My Projects</h3>
-      <a href="https://github.com/SuptitzGabriel" target="blank">Veja todos</a>
-    </div>
-    <div className='projects'>
-      <div>
-          <div className='project projectOne'>
-          <div className='nameProject'>
-             <img src="../public/assets/folder.svg" alt="Ícon Folder" />
-             <a href='https://github.com/SuptitzGabriel/-Portfolio-challenge' target="blank">-Portfolio-challenge</a>
-          </div>  
-             <p>Desafio da rockeseat,realizado com intuito de aprender mais sobre html e css,feito em react para treino.</p>
-          <div>
-            <div className='infoProject'>
-              <div className='infoGitHub'>  
-                <div> 
-                  <img src="../public/assets/star-project.svg" alt="Ícon Star" /> 
-                  <p>000</p>
-                </div>
-                <div>
-                  <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" />
-                  <p>001</p>
-                </div>
-
-               
-              </div>
-           
-              <div className='projectTec'>
-                <p className='elipseJs'></p>
-                <p>JavaScript</p>
-               
-              </div>
-            </div>
-               
-             </div>
-              </div>
-              <div className='project projectTwo'>
-          <div className='nameProject'>
-             <img src="../public/assets/folder.svg" alt="Ícon Folder" />
-             <a href='https://github.com/SuptitzGabriel/NLW-Return-' target="blank">NLW-Return-</a>
-          </div>  
-             <p>Projeto realizado na NLW Return da Rocketseat com intuito de aprender o básico de html e css, criando um site simples.</p>
-          <div>
-            <div className='infoProject'>
-              <div className='infoGitHub'>  
-                <div> 
-                  <img src="../public/assets/star-project.svg" alt="Ícon Star" /> 
-                  <p>000</p>
-                </div>
-                <div>
-                  <img src="../public/assets/git-branch.svg" alt="Ícon Git Branch" />
-                  <p>001</p>
-                </div>
-
-               
-              </div>
-           
-              <div className='projectTec'>
-             
-                <p className='elipseHtml'></p>
-                <p>HTML</p>
-               
-              </div>
-            </div>
-               
-             </div>
-              </div>
-        <div>
-
+    <main>
+      <div className="myProject">
+        <h3>My Projects</h3>
+        <a href="https://github.com/SuptitzGabriel" target="blank">
+          Veja todos
+        </a>
       </div>
-     </div>
-    </div>
-    <div className='message'>
-      <h3>Mensagem</h3>
-    </div>
-    <div className='message textMessage'>
-<div><img  id="gitImage" className='messagePic' src="https://avatars.githubusercontent.com/u/102992996?v=4" alt="Imagem de perfil" /></div>
-      
+      <div className="projects">
+        <div>
+          <div className="project projectOne">
+            <div className="nameProject">
+              <img src="../public/assets/folder.svg" alt="Ícon Folder" />
+              <a
+                href="https://github.com/SuptitzGabriel/-Portfolio-challenge"
+                target="blank"
+              >
+                -Portfolio-challenge
+              </a>
+            </div>
+            <p>
+              Desafio da rockeseat,realizado com intuito de aprender mais sobre
+              html e css,feito em react para treino.
+            </p>
+            <div>
+              <div className="infoProject">
+                <div className="infoGitHub">
+                  <div>
+                    <img
+                      src="../public/assets/star-project.svg"
+                      alt="Ícon Star"
+                    />
+                    <p>000</p>
+                  </div>
+                  <div>
+                    <img
+                      src="../public/assets/git-branch.svg"
+                      alt="Ícon Git Branch"
+                    />
+                    <p>001</p>
+                  </div>
+                </div>
 
-      <p>
-       <strong>Olá,Tudo certo✌️ ?</strong>  
-       <br />
-       <br />
-Meu nome é Gabriel Suptitz estou no caminho para me tornar um programador e tentando alcançar sempre um próximo nível 🚀, este é um portifólio feito com base no desafio do discover da Rocketseat,mas com algumas preferências próprias!
-</p>
-       
-    </div>
-  </main>
+                <div className="projectTec">
+                  <p className="elipseJs"></p>
+                  <p>JavaScript</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="project projectTwo">
+            <div className="nameProject">
+              <img src="../public/assets/folder.svg" alt="Ícon Folder" />
+              <a
+                href="https://github.com/SuptitzGabriel/NLW-Return-"
+                target="blank"
+              >
+                NLW-Return-
+              </a>
+            </div>
+            <p>
+              Projeto realizado na NLW Return da Rocketseat com intuito de
+              aprender o básico de html e css, criando um site simples.
+            </p>
+            <div>
+              <div className="infoProject">
+                <div className="infoGitHub">
+                  <div>
+                    <img
+                      src="../public/assets/star-project.svg"
+                      alt="Ícon Star"
+                    />
+                    <p>000</p>
+                  </div>
+                  <div>
+                    <img
+                      src="../public/assets/git-branch.svg"
+                      alt="Ícon Git Branch"
+                    />
+                    <p>001</p>
+                  </div>
+                </div>
+
+                <div className="projectTec">
+                  <p className="elipseHtml"></p>
+                  <p>HTML</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div className="message">
+        <h3>Mensagem</h3>
+      </div>
+      <div className="message textMessage">
+        <div>
+          <img
+            id="gitImage"
+            className="messagePic"
+            src="https://avatars.githubusercontent.com/u/102992996?v=4"
+            alt="Imagem de perfil"
+          />
+        </div>
+
+        <p>
+          <strong>Olá,Tudo certo✌️ ?</strong>
+          <br />
+          <br />
+          Meu nome é Gabriel Suptitz estou no caminho para me tornar um
+          programador e tentando alcançar sempre um próximo nível 🚀, este é um
+          portifólio feito com base no desafio do discover da Rocketseat,mas com
+          algumas preferências próprias!
+        </p>
+      </div>
+    </main>
   )
 }
 
